326f7dfcf0d6761b386e5c3a7c617ef1ebf7fba9
rossyvee <roseakinyi001@gmail.com>
2022-05-13 11:23:52 +0300

    adding the main decorator and defining the view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,2 +1,10 @@
 from flask import render_template
 from app import app
+
+@main.route('/')
+def index():
+    pitches = Pitch.query.all()
+    job = Pitch.query.filter_by(category = 'Job').all() 
+    event = Pitch.query.filter_by(category = 'Events').all()
+    advertisement = Pitch.query.filter_by(category = 'Advertisement').all()
+    return render_template('index.html', job = job,event = event, pitches = pitches,advertisement= advertisement)
