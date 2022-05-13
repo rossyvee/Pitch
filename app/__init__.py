836dc258c23e82fe2baa09c1bb9175fb7db1941a
rossyvee <roseakinyi001@gmail.com>
2022-05-13 16:01:45 +0300

    registering the blueprint and importing it from main

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,10 @@ from config import config_options
 
 def create_app(config_name):
     app = Flask(__name__)
+    from .main import main as main_blueprint
+
+    app.register_blueprint(main_blueprint)
+
     return app
 
 # from app import views
