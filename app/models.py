0b32aa7fe28d5f367f8fa2044e53971a178f9fde
rossyvee <roseakinyi001@gmail.com>
2022-05-13 16:57:28 +0300

    creating the imports for flask and SQLAlchemy

--- app/models.py
+++ app/models.py
@@ -1,4 +1,16 @@
 # model class for user
 #model class for categories
-#model for pitches
-#models for votes
+#model  class for pitches
+#models class for votes
+#model class for comments
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
