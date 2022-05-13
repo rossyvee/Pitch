c265e1ac226164f33ea9277b5dc61b5fc2ad2be8
rossyvee <roseakinyi001@gmail.com>
2022-05-13 15:57:42 +0300

    adding the create app function

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from  flask import Flask
+from config import config_options
 
-app = Flask(__name__)
+def create_app(config_name):
+    app = Flask(__name__)
+    return app
 
-from app import view
+# from app import views
