f8597bfd9d7be0586287c143b63d9048e7ef9801
rossyvee <roseakinyi001@gmail.com>
2022-05-14 10:36:45 +0300

    adding functions to the login page

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,28 @@
 
-from flask import render_template
-from . import auth
+from flask import render_template, request, redirect, url_for
+from flask_login import login_user, logout_user
+from app.auth import auth
 
 @auth.route('/login')
 def login():
-    return "Login"
+    if request.method == 'POST':
+        form = request.form
+        username = form.get('username')
+        password = form.get('password')
+        print(username)
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            error = 'A user with that username  does not exist'
+            return render_template('login.html', error=error)
+        is_correct_password = user.check_password(password)
+        print(is_correct_password)
+        if not is_correct_password:
+            error = 'A user with that password does not exist'
+            return render_template('login.html', error=error)
+        login_user(user)
+        return redirect('/')
+    return render_template('login.html')
+
 
 @auth.route('/sign-up')
 def sign_up():
