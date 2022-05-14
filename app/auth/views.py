914a7da0e6f8a50d95a531ae6252d982488e76ee
rossyvee <roseakinyi001@gmail.com>
2022-05-14 11:20:52 +0300

    updating the signup functions

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,61 +1,34 @@
 
 from flask import render_template, request, redirect, url_for
+from app import auth
 from flask_login import login_user, logout_user
-from app.auth import auth
+
+
 
 @auth.route('/login')
 def login():
-    if request.method == 'POST':
-        form = request.form
-        username = form.get('username')
-        password = form.get('password')
-        print(username)
-        user = User.query.filter_by(username=username).first()
-        if user is None:
-            error = 'user does not exist'
-            return render_template('login.html', error=error)
-        is_correct_password = user.check_password(password)
-        print(is_correct_password)
-        if not is_correct_password:
-            error = ' password error'
-            return render_template('login.html', error=error)
-        login_user(user)
-        return redirect('/')
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username = form.username.data).first()
+        if user != None and user.verify_password(form.password.data):
+            login_user(user,form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or Password')
+    return render_template('auth/login.html', loginform = form)
 
 
 @auth.route('/sign-up')
 def sign_up():
- if request.method == 'POST':
-        form = request.form
-        username = form.get("username")
-        email = form.get("email")
-        password = form.get("password")
-        confirm_password = form.get("confirm_password")
-        if username is None or password is None or email is None or confirm_password is None:
-            error = 'username, email, password are required'
-            return render_template('signup.html', error=error)
-        if ' ' in username:
-            error = 'Username should not contain spaces'
-            return render_template('signup.html', error=error)
-        if password != confirm_password:
-            error = "Passwords do not match"
-            return render_template('signup.html', error=error)
-        else:
-            user = User.query.filter_by(username=username).first()
-            if user is not None:
-                error = 'A user with that name already exists'
-                return render_template('signup.html', error=error)
-            user = User.query.filter_by(email=email).first()
-            if user is not None:
-                error = 'A user with that email already exists'
-                return render_template('signup.html', error=error)
-            user = User(username=username, email=email)
-            user.set_password(password)
-            user.save()
-            # email_message("Welcome to 1m Pitch", "email/howdy", user.email, user=user)
-            return redirect(url_for('auth.login'))
+   form = RegForm()
+   if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data, password = form.password.data)
+        user.save_u()
+        mail_message("Welcome to Pitch-World","email/welcome_user",user.email,user=user)
+        return redirect(url_for('auth.login'))
+        return render_template('auth/signup.html', r_form = form)
+
 
 @auth.route('/sign-out')
 def login_out():
+
    return render_template('log_out.html')
