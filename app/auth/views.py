44821b73ca441a746dce8e24917ca4887fb6abd5
rossyvee <roseakinyi001@gmail.com>
2022-05-14 10:45:52 +0300

    adding the sign up functions

--- app/auth/views.py
+++ app/auth/views.py
@@ -12,12 +12,12 @@ def login():
         print(username)
         user = User.query.filter_by(username=username).first()
         if user is None:
-            error = 'A user with that username  does not exist'
+            error = 'user does not exist'
             return render_template('login.html', error=error)
         is_correct_password = user.check_password(password)
         print(is_correct_password)
         if not is_correct_password:
-            error = 'A user with that password does not exist'
+            error = ' password error'
             return render_template('login.html', error=error)
         login_user(user)
         return redirect('/')
@@ -26,8 +26,36 @@ def login():
 
 @auth.route('/sign-up')
 def sign_up():
-    return "Sign-up"
+ if request.method == 'POST':
+        form = request.form
+        username = form.get("username")
+        email = form.get("email")
+        password = form.get("password")
+        confirm_password = form.get("confirm_password")
+        if username is None or password is None or email is None or confirm_password is None:
+            error = 'username, email, password are required'
+            return render_template('signup.html', error=error)
+        if ' ' in username:
+            error = 'Username should not contain spaces'
+            return render_template('signup.html', error=error)
+        if password != confirm_password:
+            error = "Passwords do not match"
+            return render_template('signup.html', error=error)
+        else:
+            user = User.query.filter_by(username=username).first()
+            if user is not None:
+                error = 'A user with that name already exists'
+                return render_template('signup.html', error=error)
+            user = User.query.filter_by(email=email).first()
+            if user is not None:
+                error = 'A user with that email already exists'
+                return render_template('signup.html', error=error)
+            user = User(username=username, email=email)
+            user.set_password(password)
+            user.save()
+            # email_message("Welcome to 1m Pitch", "email/howdy", user.email, user=user)
+            return redirect(url_for('auth.login'))
 
 @auth.route('/sign-out')
 def login_out():
-    return "Log-out"
+   return render_template('log_out.html')
