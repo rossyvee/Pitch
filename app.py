e7d902c2f9141b101bd992260b7beff1a8670651
rossyvee <roseakinyi001@gmail.com>
2022-05-14 06:42:08 +0300

    updating the folder

---
+++ app.py
@@ -0,0 +1,7 @@
+from distutils.log import debug
+from app import create_app
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,11 @@
-from  flask import Flask
-from config import config_options
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
 
-def create_app(config_name):
+def create_app(app):
     app = Flask(__name__)
-    from .main import main as main_blueprint
+    app.config['SECRET_KEY'] = "hello world"
 
-    app.register_blueprint(main_blueprint)
 
     return app
-
-# from app import views
