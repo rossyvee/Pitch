41c3b8f572c8e125e17ff0cf23c4bfae2abb9743
rossyvee <roseakinyi001@gmail.com>
2022-05-14 17:28:52 +0300

    installing the wtf form and imorting it to the forms file

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,16 @@
 
-from flask import render_template
-from app import main
+from flask import render_template, redirect, url_for,abort,request
+from . import main
+from flask_login import login_required,current_user
+from ..models import User,Pitch,Comment,Upvote,Downvote
+from .forms import UpdateProfile,PitchForm,CommentForm
+from .. import db
 
 
 @main.route('/')
 def home():
-    return render_template(".html")
-    
+    pitches = Pitch.query.all()
+    business = Pitch.query.filter_by(category = 'Job').all() 
+    technology = Pitch.query.filter_by(category = 'Events').all()
+    idea = Pitch.query.filter_by(category = 'Advertisement').all()
+    return render_template('index.html', business = business,technology = technology, pitches = pitches,idea= idea)
