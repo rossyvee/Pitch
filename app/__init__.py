91477eb5d68d49086f7c6a5018d804e9874a03de
rossyvee <roseakinyi001@gmail.com>
2022-05-14 07:41:29 +0300

    importing auth and registering a blueprint for auth

--- app/__init__.py
+++ app/__init__.py
@@ -9,8 +9,9 @@ def create_app(app):
     app.config['SECRET_KEY'] = "hello world"
 
     from .main import main as main_blueprint
+    from .auth import auth as auth_blueprint
+    
     app.register_blueprint(main_blueprint)
-
-
+    app.register_blueprint(auth_blueprint)
 
     return app
