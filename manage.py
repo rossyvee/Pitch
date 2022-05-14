bcaec23090965d90149def059f0fcd192ae4ae7a
rossyvee <roseakinyi001@gmail.com>
2022-05-14 17:56:16 +0300

    creating the environment variables from dotenv

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from app import create_app,db
 from app.models import User
-from app import create_app
+
 
 
 manager = Manager(app)
