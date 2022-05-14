4686994ec38afbebff2e4f53839e39f602620bfc
rossyvee <roseakinyi001@gmail.com>
2022-05-14 15:48:50 +0300

    creating the downvote and defining the functions

--- app/models.py
+++ app/models.py
@@ -85,9 +85,6 @@ class Upvote(db.Model):
         def __repr__(self):
          return f'Pitch {self.post}'
 
-
-
-
 class Comment(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
@@ -108,3 +105,25 @@ class Comment(db.Model):
     
     def __repr__(self):
         return f'comment:{self.comment}'
+
+class Downvote(db.Model):
+        __tablename__ = 'downvotes'
+
+        id = db.Column(db.Integer,primary_key=True)
+        user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+        pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    
+
+        def save(self):
+            db.session.add(self)
+            db.session.commit()
+        @classmethod
+        def get_downvotes(cls,id):
+            downvote = Downvote.query.filter_by(pitch_id=id).all()
+            return downvote
+
+        def __repr__(self):
+            return f'{self.user_id}:{self.pitch_id}'
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
