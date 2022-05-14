1f7fdf8f3a8e4a07eb0b4e722787b8dc2249060f
rossyvee <roseakinyi001@gmail.com>
2022-05-14 17:25:13 +0300

    adding the database and the variables that stores user,delete then commit the sessions

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from flask_login import UserMixin, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db,login_manager
+import app
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
