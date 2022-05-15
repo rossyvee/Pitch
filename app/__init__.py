0cee4664b8ca7a77d34e654450f877b39f527f95
rossyvee <roseakinyi001@gmail.com>
2022-05-15 06:43:40 +0300

    importing the user table from models in the app

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,12 @@ from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from config import config_options
-
+from .models import user
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
-from app.models import User
 mail = Mail()
 bootstap = Bootstrap()
 
