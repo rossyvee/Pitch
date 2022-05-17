5179f37b475bd3abe6ca501eda6beec6d36aa975
rossyvee <roseakinyi001@gmail.com>
2022-05-17 16:04:02 +0300

    updating changes to the workspace

--- config.py
+++ config.py
@@ -23,8 +23,7 @@ class Config:
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL")
-
+    SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL").replace('postgres://', 'postgresql://')
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_DATABASE_URL")
     DEBUG = True
